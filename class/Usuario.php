83bf5dff0dad8e2d08275cc6f4666ade0a4d3d66
Reyson Afonso <reysonafonso1979@gmail.com>
2017-09-30 17:42:33 -0400

    Metodos de Lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -40,15 +40,47 @@ class Usuario {
 
 	public function loadById($id){
 		$sql = new Sql();
-		$result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 			":ID" => $id));
 
-		if(count($results[0]) > 0){
+		if(count($results) > 0){
 			$row = $results[0];
-			$this->setIdusuario($row["idusuario"]);
-			$this->setDeslogin($row["deslogin"]);
-			$this->setDesenha($row["dessenha"]);
-			$this->setDtcadastro(new DateTime($row["dtcadastro"]));
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+	}
+
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+			));
+	}
+
+	public function login($login, $senha){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN" => $login,
+			":PASSWORD" => $senha
+		));
+
+		if(count($results) > 0){
+			$row = $results[0];
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+			throw new Exception("Login e/ou senha inválidos");
+			
 		}
 	}
 
@@ -57,7 +89,7 @@ class Usuario {
 			"idusuario" => $this->getIdusuario(),
 			"deslogin" => $this->getDeslogin(),
 			"dessenha" => $this->getDessenha(),
-			"dtcadastro" => $this->getDtcadastro()->format("d/m/y H:i:s")
+			"dtcadastro" => $this->getDtcadastro()->format('d/m/Y H:i:s')
 		));
 	}
 }
--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuário
+// $root = new Usuario();
+// $root->loadbyId(4);
+// echo $root;
 
-$root->loadbyId(4);
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
 
-echo $root;
+// echo json_encode($lista);
 
+// Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search('reyson');
+
+//echo json_encode($search);
+
+// carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+
+$usuario->login('reyson', '0sn0f@');
+
+echo $usuario;
