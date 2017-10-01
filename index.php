8aebeca5e21435c2709566a4bd65119f09bf99e2
Reyson Afonso <reysonafonso1979@gmail.com>
2017-09-30 22:51:10 -0400

    Inserir e Atualizar Dados

--- index.php
+++ index.php
@@ -18,8 +18,24 @@ require_once("config.php");
 //echo json_encode($search);
 
 // carrega um usuario usando o login e a senha
-$usuario = new Usuario();
+//$usuario = new Usuario();
+//$usuario->login('reyson', '0sn0f@');
+//echo $usuario;
 
-$usuario->login('reyson', '0sn0f@');
+
+// Criando um novo usuario
+// $aluno = new Usuario("leticia","leticianismo");
+
+//$aluno->setDeslogin('suelen');
+//$aluno->setDessenha('sussuca');
+
+// $aluno->insert();
+
+// echo $aluno;
+
+$usuario = new Usuario();
+$usuario->loadbyId(4);
+
+$usuario->update("Denis", "denis120676");
 
 echo $usuario;
